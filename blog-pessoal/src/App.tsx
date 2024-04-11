2ec6ac4e76bc536e22fe8ad76485b93ae68aa7c7
Jonathan Garuti <jonathan_garuti@hotmail.com>
2024-04-10 22:38:21 -0300

    Adicionado componentes de navbar e footer

--- blog-pessoal/src/App.tsx
+++ blog-pessoal/src/App.tsx
@@ -1,9 +1,17 @@
 import React from "react";
-import Home from "./paginas/home/Home";
 import "./App.css";
+import Home from "./paginas/home/Home";
+import Navbar from "./components/navBar/NavBar";
+import Footer from "./components/footer/Footer";
+import Login from "./paginas/login/Login";
 
 function App() {
-  return <Home />;
+  return (
+    <>
+      <Navbar />
+      <Home />
+      <Footer />
+    </>
+  );
 }
-
 export default App;

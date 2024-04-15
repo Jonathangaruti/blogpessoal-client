78bc932a6ba611af65ce2b98d6876b8916ad8c8a
Jonathan Garuti <jonathan_garuti@hotmail.com>
2024-04-15 18:44:55 -0300

    Implementação dos métodos HTTP para intereção com o backend

--- blog-pessoal/src/App.tsx
+++ blog-pessoal/src/App.tsx
@@ -6,8 +6,11 @@ import Footer from "./components/footer/Footer";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./paginas/login/Login";
 import Cadastro from "./paginas/cadastro/Cadastro";
-
 import { AuthProvider } from "./contexts/AuthContext";
+
+import ListaTemas from "./components/temas/listaTemas/ListaTemas";
+import FormularioTema from "./components/temas/formularioTema/FormularioTema";
+import DeletarTema from "./components/temas/deletarTema/DeletarTema";
 
 function App() {
   return (
@@ -21,6 +24,10 @@ function App() {
               <Route path="/login" element={<Login />} />
               <Route path="/cadastro" element={<Cadastro />} />
               <Route path="/home" element={<Home />} />
+              <Route path="/temas" element={<ListaTemas />} />
+              <Route path="/cadastroTema" element={<FormularioTema />} />
+              <Route path="/editarTema/:id" element={<FormularioTema />} />
+              <Route path="/deletarTema/:id" element={<DeletarTema />} />
             </Routes>
           </div>
           <Footer />
